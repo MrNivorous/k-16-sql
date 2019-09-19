bbca6e535cfc93fe2f6c118ee3e89138df58e71b
MrNivorous <karn.sood@gmail.com>
2019-09-19 15:00:38 +0700

    sql katas done

--- k-16.sql
+++ k-16.sql
@@ -30,7 +30,7 @@ WHERE city = 'Samui'
 
 --160205
 SELECT * FROM places
-WHERE price >= 80 AND price <=150
+WHERE price BETWEEN 80 AND 150
 
 --160206
 SELECT id, title, price FROM places
@@ -47,3 +47,59 @@ WHERE rating = 5 OR rating = 3
 --160209
 SELECT * FROM users
 WHERE email LIKE '%gmail.com'
+
+--160301
+SELECT * FROM places
+LEFT JOIN types
+ON places.type = types.id
+
+--160302
+SELECT places.id, places.title, places.price, types.name AS type FROM places
+LEFT JOIN types
+ON places.type = types.id
+
+--160303
+SELECT places.id, places.title, places.price, types.name AS type,
+	users.name AS host,users.email FROM places
+LEFT JOIN types
+ON places.type = types.id
+LEFT JOIN users
+ON places.host = users.id
+WHERE places.id < 6
+
+--160304
+SELECT users.name, users.email, places.title, places.rating FROM users
+LEFT JOIN places
+ON users.id = places.host
+
+--160305
+SELECT users.name, users.email, places.title, places.rating FROM users
+INNER JOIN places
+ON users.id = places.host
+ORDER BY places.id
+
+--160306
+SELECT places.title, users.name AS fan FROM places
+LEFT JOIN likes
+ON places.id = likes.place
+LEFT JOIN users
+ON likes.user = users.id
+ORDER BY places.title
+
+--160307
+SELECT places.title, users.name AS fan FROM places
+INNER JOIN likes
+ON places.id = likes.place
+INNER JOIN users
+ON likes.user = users.id
+ORDER BY users.name, places.type
+
+--160308
+SELECT users.name AS fan, places.title AS place, types.name as type FROM places
+INNER JOIN likes
+ON places.id = likes.place
+INNER JOIN users
+ON likes.user = users.id
+INNER JOIN types
+ON types.id = places.type
+ORDER BY users.name, places.type
